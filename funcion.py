9cf3d6e1ade2f1297f366d181c104eb3bc5203a0
kfajardop <kfajardop@miumg.edu.gt>
2023-10-25 18:16:19 -0600

    funcion2
    acción

---
+++ funcion.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Crear una ventana de Tkinter
+ventana = tk.Tk()
+ventana.title("Cambio de color de fondo")
+
+# Configurar el color de fondo de la ventana
+ventana.configure(bg="blue")  # Puedes cambiar "blue" al color que desees
+
+# Añadir elementos a la ventana (por ejemplo, etiquetas, botones, etc.)
+
+# Mostrar la ventana
+ventana.mainloop()
